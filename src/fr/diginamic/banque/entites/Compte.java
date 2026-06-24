f7e7e2a8346018f880f292315c33bae2a8d6bff7
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-24 12:08:27 +0200

    TP6 - METHODE REDEFINIE ET HERITAGE fini

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -28,4 +28,9 @@ public class Compte {
         this.numCompte = numCompte;
         this.soldeCompte = soldeCompte;
     }
+
+    @Override
+    public String toString() {
+        return "Numéro de Compte : " + numCompte + "\nSolde : " + soldeCompte;
+    }
 }
